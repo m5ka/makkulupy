20ddb521c69c10def68ac798556dab288d8b6ae2
Marzena <git@marzka.cafe>
2023-02-18 21:56:05 +0000

    🔥 export models from makkulupy module

--- makkulupy/__init__.py
+++ makkulupy/__init__.py
@@ -1,4 +1,6 @@
 __version__ = "0.1.0"
 
 from .exceptions import BadResponseException, MalformedResponseException
+from .models.batch import Batch, BatchSet
+from .models.word import Word, WordSet
 from .requests import get_batches, get_words
