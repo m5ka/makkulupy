116a5af50effa4ec6a39727d8c09ed9400811205
Marzena <git@marzka.cafe>
2023-02-19 12:19:25 +0000

    🎨 appease almighty linter

--- docs/conf.py
+++ docs/conf.py
@@ -6,31 +6,30 @@
 import sys
 from os import path
 
-sys.path.insert(0, path.abspath('..'))
+sys.path.insert(0, path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'makkulupy'
-copyright = '2023, marzka'
-author = 'marzka'
+project = "makkulupy"
+copyright = "2023, marzka"
+author = "marzka"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
